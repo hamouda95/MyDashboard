dbef278cc61802b781244721db52570c4e6c95af
Ben Makhlouf Mohamed <hamouda95@users.noreply.github.com>
2020-06-01 02:35:00 +0100

    Initial commit. spring boot project and configure application properties

---
+++ MyDashboard/src/main/java/Vermeg/MyDashboard/MyDashboardApplication.java
@@ -0,0 +1,13 @@
+package Vermeg.MyDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDashboardApplication.class, args);
+	}
+
+}
